f8076ae92323763820681f584d91e6414845e768
Mikael Goglichidze <goglichidze.mike@gmail.com>
2022-01-31 11:29:36 +0400

    added new excel export with headisngs

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -7,10 +7,11 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 Use PhpOffice\PhpSpreadsheet\Shared\Date;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
 
-class UsersExport implements FromCollection, WithColumnFormatting, WithMapping
+class UsersExport implements FromCollection, WithColumnFormatting, WithMapping, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -20,14 +21,28 @@ class UsersExport implements FromCollection, WithColumnFormatting, WithMapping
 	    {
 	        return [
 	            Date::dateTimeToExcel($row->created_at),
-	            Date::dateTimeToExcel($row->updated_at),
+                'name' => $row['name'],
+                'email' => $row->email,
+                'phone' => $row->phone,
+                'message' => $row->message,
+                'referrer' => $row->referrer
 	        ];
 	    }
+    public function headings(): array
+    {
+        return [
+            'created_at',
+            'name',
+            'email',
+            'phone',
+            'message',
+            'referrer'
+        ];
+    }
 	
     public function columnFormats(): array {
         return [
             'A' => NumberFormat::FORMAT_DATE_DDMMYYYY,
-            'B' => NumberFormat::FORMAT_DATE_DDMMYYYY
         ];
     }
 
